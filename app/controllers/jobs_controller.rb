f3f11c662164b9fb14b1e082bfcb3b867ac64899
Katrin Winiarski <winiarski.katrin@gmail.com>
2017-10-23 20:09:48 +0200

    many many changes

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,30 @@
+class JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update_attributes(job_params)
+    redirect_to jobs_path
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    Job.create(job_params)
+    redirect_to new_job_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :from, :to, :employer)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about_me', to: 'pages#index'
   get 'home', to: 'pages#home'
+  resources :jobs
 end
